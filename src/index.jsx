bfeb9f5b20df4241f8f39338d02e02d1626e2ed9
PythonerX5 <onuruyar1810@gmail.com>
2025-01-03 09:40:13 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const Countdown = () => {
-    const HedefZaman = new Date('2025-01-03T11:00:00');
+    const HedefZaman = new Date('2025-01-03T10:30:00');
     const [timeLeft, setTimeLeft] = useState({});
     const [timeIsUp, setTimeIsUp] = useState(false);
 
